0d740b8e18588f95886784bfb088c11af0ca50c2
Sheamus <zhoujunfeng@boco.com.cn>
2018-07-03 22:28:36 +0800

    数组数据结构的提交

---
+++ src/main/java/org/sheamus/datastructure/array/TestIntArray.java
@@ -0,0 +1,12 @@
+package org.sheamus.datastructure.array;
+
+/**
+ * int型的数组
+ * Created by Sheamus on 2018/7/3.
+ */
+public class TestIntArray {
+    public static void main(String[] args) {
+        // 给数组分配空间，Java中的数组是引用类型
+        int[] myArray = new int[10];
+    }
+}
