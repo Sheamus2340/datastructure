73bc0548cbc469592fe04f4d5a5ef9eb575af1a0
Sheamus <377890470@qq.com>
2023-08-13 10:01:47 +0800

    算法数组的练习

---
+++ src/main/java/org/sheamus/learn/l23/base/link/DeleteDuplicates.java
@@ -0,0 +1,19 @@
+package org.sheamus.learn.l23.base.link;
+
+public class DeleteDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+
+
+}
